e75295f1141a0d9e5e0ae6cb94e4238222deb4dc
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-09-12 11:55:10 +0530

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { PrismaModule } from './prisma/prisma.module.js';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AppService } from './app.service.js';
       isGlobal: true,
       cache: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
