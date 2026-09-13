b30e8441209fe03a45787f3537b1759a73c9cd1d
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-09-13 08:59:37 +0530

    Create reviews.module.ts

---
+++ backend/src/reviews/reviews.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+import { AuthModule } from '../auth/auth.module.js';
+import { ReviewsController } from './reviews.controller.js';
+import { ReviewsService } from './reviews.service.js';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [ReviewsController],
+  providers: [ReviewsService],
+  exports: [ReviewsService],
+})
+export class ReviewsModule {}
